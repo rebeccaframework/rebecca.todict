ee7db147e94cb01d8b91e162602fe3309f26e173
Atsushi Odagiri <aodagx@gmail.com>
2013-02-16 12:27:38 +0900

    implements component

--- src/rebecca/todict/__init__.py
+++ src/rebecca/todict/__init__.py
@@ -1,1 +1,31 @@
-# This package may contain traces of nuts
+from pyramid.interfaces import IDict, IRequest
+
+def set_todict(config, type, callable, name=""):
+    type = config.maybe_dotted(type)
+    callable = config.maybe_dotted(callable)
+    reg = config.registry
+
+    def register():
+        reg.registerAdapter(callable,
+                            [IRequest, type],
+                            IDict,
+                            name=name)
+
+    intr = config.introspectable(category_name='todict',
+                                 discriminator='todict of {0}'.format(type.__name__),
+                                 title='todict of {0}'.format(type.__name__),
+                                 type_name=None)
+
+    config.action("todict", register,
+                  introspectables=(intr,))
+
+
+def todict(request, value, name=""):
+    adapted = request.registry.getMultiAdapter([request, value], IDict)
+    return adapted
+
+
+def includeme(config):
+    config.add_directive('set_todict',
+                         set_todict)
+
--- src/rebecca/todict/test_todict.py
+++ src/rebecca/todict/test_todict.py
@@ -1,2 +1,21 @@
-def test_it():
+from pyramid import testing
+
+class Dummy(object):
     pass
+
+def to_dict_dummy(request, obj):
+    return dict(value="test")
+
+def test_it():
+    from pyramid.interfaces import IRequest, IDict
+    from . import set_todict, todict
+    config = testing.setUp()
+    set_todict(config, Dummy, to_dict_dummy)
+
+    dummy = Dummy()
+    request = testing.DummyRequest()
+    result = todict(request, dummy)
+    
+    assert result == dict(value="test")
+
+
